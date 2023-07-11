dd0b6ccd18b479987c758521434cc1590f6f133a
kevincw456 <119997828+kevincw456@users.noreply.github.com>
2023-07-11 17:54:29 +0800

    changeNew

--- demo/settings.py
+++ demo/settings.py
@@ -80,14 +80,14 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': '0UcscXzEQXOPqUtCrigQ',
-        'HOST': 'containers-us-west-24.railway.app',
-        'PORT': '5810',
-    }
+#   'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'railway',
+#        'USER': 'postgres',
+#        'PASSWORD': '0UcscXzEQXOPqUtCrigQ',
+#        'HOST': 'containers-us-west-24.railway.app',
+#        'PORT': '5810',
+#    }
 }
 
 
